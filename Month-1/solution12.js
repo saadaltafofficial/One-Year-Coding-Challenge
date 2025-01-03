2d7fa2536dc6fdd55491b7d7cbcf339c2d6ea130
Saad Altaf <134957807+saadaltafofficial@users.noreply.github.com>
2025-01-03 10:29:24 +0500

    Update solution12.js

--- Month-1/solution12.js
+++ Month-1/solution12.js
@@ -13,4 +13,18 @@ function findSmallestNumber(Arr){
   return smallestNumber
 }
 
+function findSmallestNumber(Arr){
+  if(Arr.length === 0){
+    return null
+  }
+  let smallestNumber = Arr[0]
+  for(let i=0; i < Arr.length; i++){
+    if(Arr[i] < smallestNumber){
+      smallestNumber = Arr[i]
+    }
+  }
+  return smallestNumber
+}
+
+console.log(findSmallestNumber(numbersArray))
 console.log(findSmallestNumber(numbersArray))
